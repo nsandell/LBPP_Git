97174c97419899a0bd1ec7f96c7d1a7078e2b7b7
nsandell <nsandell@d05d5076-8e11-4e8a-86b5-5794bff6af64>
2010-10-07 17:36:20 +0000

    Comments

--- bn/messages/DiscreteMessage.java
+++ bn/messages/DiscreteMessage.java
@@ -2,28 +2,32 @@ package bn.messages;
 
 import bn.BNException;
 
+/**
+ * Class that implements message functionality between discrete nodes
+ * and nodes that can be connected to discrete nodes.
+ * @author Nils F. Sandell
+ */
 public class DiscreteMessage
 {
+	/**
+	 * Creates a message with desired cardinality.
+	 * @param cardinality Cardinality.
+	 */
 	public DiscreteMessage(int cardinality)
 	{
 		this.message_values = new double[cardinality];
 		for(int i = 0; i < cardinality; i++)
 			this.message_values[i] = 0;
 		this.cardinality = cardinality;
-		this.isDelta = false;
 	}
 	
-	public DiscreteMessage(int cardinality, int deltaIndex)
-	{
-		this.cardinality = cardinality;
-		this.deltaIndex = deltaIndex;
-		this.isDelta = true;
-	}
-	
+	/**
+	 * Normalize this message to sum to 1. If this
+	 * message is all 0s it stays that way and returns 0.
+	 * @return The sum that was used to normalize.
+	 */
 	public double normalize()
 	{
-		if(this.isDelta)
-			return 1;
 		double sum = 0;
 		for(int i = 0; i < message_values.length; i++)
 			sum += message_values[i];
@@ -33,50 +37,45 @@ public class DiscreteMessage
 		return sum;
 	}
 	
+	/**
+	 * Perform entry-wise division between this message and an argument message,
+	 * storing the result in a new message.
+	 * @param other The other message (the denominator).
+	 * @return Resultant message
+	 * @throws BNException If the two operands are of unequal length.
+	 */
 	public DiscreteMessage divide(DiscreteMessage other) throws BNException
 	{
 		if(other.cardinality!=this.cardinality)
 			throw new BNException("Attempted to multiply messages of unequal length.");
-		if(this.isDelta)
-		{
-			DiscreteMessage ret = new DiscreteMessage(this.cardinality);
-			ret.setValue(this.deltaIndex, 1.0/other.getValue(this.deltaIndex));
-			return ret;
-		}
-		else
-		{
-			DiscreteMessage ret = new DiscreteMessage(this.cardinality);
-			for(int i = 0; i < this.cardinality; i++)
-				ret.setValue(i, this.message_values[i]/other.message_values[i]);
-			return ret;
-		}
+		DiscreteMessage ret = new DiscreteMessage(this.cardinality);
+		for(int i = 0; i < this.cardinality; i++)
+			ret.setValue(i, this.message_values[i]/other.message_values[i]);
+		return ret;
 	}
-	
+
+	/**
+	 * Perform entry-wise multiplication between this message and an argument message,
+	 * storing the result in a new message.
+	 * @param other The other message (the denominator).
+	 * @return Resultant message
+	 * @throws BNException If the two operands are of unequal length.
+	 */
 	public DiscreteMessage multiply(DiscreteMessage other) throws BNException
 	{
 		if(other.cardinality!=this.cardinality)
 			throw new BNException("Attempted to multiply messages of unequal length.");
-		if(this.isDelta || other.isDelta)
-		{
-			double val = other.getValue(this.deltaIndex);
-			if(val==1)
-				return new DiscreteMessage(this.cardinality, this.deltaIndex);
-			else
-			{
-				DiscreteMessage ret = new DiscreteMessage(this.cardinality);
-				ret.setValue(this.deltaIndex, val);
-				return ret;
-			}
-		}
-		else
-		{
-			DiscreteMessage ret = new DiscreteMessage(this.cardinality);
-			for(int i = 0; i < message_values.length; i++)
-				ret.message_values[i] = this.message_values[i] * other.message_values[i];
-			return ret;
-		}
+		DiscreteMessage ret = new DiscreteMessage(this.cardinality);
+		for(int i = 0; i < message_values.length; i++)
+			ret.message_values[i] = this.message_values[i] * other.message_values[i];
+		return ret;
 	}
 	
+	/**
+	 * Get a message consisting of all ones.
+	 * @param card The cardinality of the message.
+	 * @return All-ones message.
+	 */
 	public static DiscreteMessage allOnesMessage(int card)
 	{
 		DiscreteMessage ret = new DiscreteMessage(card);
@@ -85,39 +84,35 @@ public class DiscreteMessage
 		return ret;
 	}
 	
+	/**
+	 * Set the value of this message.
+	 * @param index Index at which to set.
+	 * @param value Value to set.
+	 */
 	public void setValue(int index, double value)
 	{
-		if(this.isDelta && (index!=deltaIndex || value!=1))
-		{
-			this.isDelta = false;
-			this.message_values = new double[cardinality];
-			this.message_values[deltaIndex] = 1;
-		}
 		this.message_values[index] = value;
 	}
 	
+	/**
+	 * Get the cardinality of this message.	
+	 * @return Integer cardinality
+	 */
 	public int getCardinality()
 	{
 		return this.cardinality;
 	}
 	
+	/**
+	 * Get the value of this message
+	 * @param index At this index
+	 * @return The value
+	 */
 	public double getValue(int index)
 	{
-		if(!this.isDelta)
-		{
-			return this.message_values[index];
-		}
-		else
-		{
-			if(index==deltaIndex)
-				return 1;
-			else
-				return 0;
-		}
+		return this.message_values[index];
 	}
 
 	private int cardinality;
-	private boolean isDelta;
-	private int deltaIndex;
 	private double[] message_values;
 }

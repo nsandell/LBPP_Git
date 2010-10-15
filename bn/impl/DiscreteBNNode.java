811676aafbc4c9a6b632ee308cab5ccaf94d0520
nsandell <nsandell@d05d5076-8e11-4e8a-86b5-5794bff6af64>
2010-10-15 16:30:15 +0000

    Fixed bug where nodes were returning likelihood, rather than log-likelihood.

--- bn/impl/DiscreteBNNode.java
+++ bn/impl/DiscreteBNNode.java
@@ -164,7 +164,8 @@ class DiscreteBNNode extends BNNode implements DiscreteParentSubscriber, Discret
 			this.local_pi.setValue(i, 0);
 	
 		Integer valueTmp = (this.observed) ? this.value : null;
-			this.likelihoodGivenPast = this.cpt.computeLocalPi(this.local_pi, this.incomingPiMessages, this.parents_local_pis, valueTmp);
+		// Need to evaluate whether it's worth it to change CPD computeLocalPis to return log likelihood rather than likelihood
+		this.likelihoodGivenPast = Math.log(this.cpt.computeLocalPi(this.local_pi, this.incomingPiMessages, this.parents_local_pis, valueTmp));
 	}
 	
 	protected void updateLambdas() throws BNException
---
+++ tests/ModifyAndLearn.java
@@ -0,0 +1,41 @@
+package tests;
+
+import bn.BNException;
+import bn.IDynBayesNet;
+import bn.IDynBayesNode;
+import bn.distributions.DiscreteCPT;
+import bn.distributions.DiscreteCPTUC;
+import bn.distributions.ScalarNoisyOr;
+
+public class ModifyAndLearn {
+	public static void main(String[] args) throws BNException
+	{
+		
+		double[][] A = new double[][]{{.9, .1},{.1, .9}};
+		double[] pi = new double[]{.5,.5};
+		IDynBayesNet net = bn.impl.BayesNetworkFactory.getDynamicNetwork(100);
+		DiscreteCPT cpt = new DiscreteCPT(A, 2);
+		DiscreteCPTUC pidist = new DiscreteCPTUC(pi);
+		
+		IDynBayesNode x = net.addDiscreteNode("X", 2);
+		net.setInitialDistribution("X", pidist);
+		net.setDistribution("X", cpt);
+		net.addInterEdge(x, x);
+		IDynBayesNode y1 = net.addDiscreteNode("Y1", 2);
+		net.addIntraEdge(x,y1);
+		net.setDistribution("Y1", new ScalarNoisyOr(.9));
+		IDynBayesNode y2 = net.addDiscreteNode("Y2", 2);
+		net.addIntraEdge(x,y2);
+		net.setDistribution("Y2", new ScalarNoisyOr(.9));
+		
+		net.validate();
+		net.optimize_parallel(20, 1e-4, 20, 1e-4);
+		
+		IDynBayesNode x2 = net.addDiscreteNode("X2", 2);
+		net.setInitialDistribution("X2", pidist);
+		net.setDistribution("X2", cpt);
+		net.addIntraEdge(x2, y1);
+		
+		net.optimize_parallel(20, 1e-4, 20, 1e-4);
+	}
+}

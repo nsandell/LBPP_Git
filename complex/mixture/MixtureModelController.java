71f20d668827192dfdc81f61232279a1cc28c12e
nsandell <nsandell@d05d5076-8e11-4e8a-86b5-5794bff6af64>
2011-02-24 18:01:43 +0000

    Fixed mixture appears to be working...

---
+++ complex/mixture/MixtureModelController.java
@@ -0,0 +1,74 @@
+package complex.mixture;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Vector;
+
+import complex.CMException;
+import complex.featural.IChildProcess;
+import complex.featural.IParentProcess;
+
+public abstract class MixtureModelController extends complex.ModelController{
+	
+	public MixtureModelController(Collection<? extends IChildProcess> children)
+	{
+		this.allChildren = new Vector<IChildProcess>(children);
+	}
+	
+	public IParentProcess getParent(IChildProcess child)
+	{
+		return this.parents.get(child);
+	}
+	
+	public Vector<IChildProcess> getChildren(IParentProcess parent)
+	{
+		return this.children.get(parent);
+	}
+	
+	public void setParent(IChildProcess child, IParentProcess parent) throws CMException
+	{
+		this.setParentI(child, parent);
+		if(parents.get(child)!=null)
+			this.children.get(parents.get(child)).remove(child);
+		this.children.get(parent).add(child);
+		this.parents.put(child, parent);
+	}
+	
+	public Vector<IChildProcess> getAllChildren()
+	{
+		return this.allChildren;
+	}
+	
+	public Vector<IParentProcess> getAllParents()
+	{
+		return this.allParents;
+	}
+	
+	public abstract void optimizeChildParameters(IChildProcess child) throws CMException;
+	
+	public void deleteParent(IParentProcess parent) throws CMException
+	{
+		this.deleteParentI(parent);
+		for(IChildProcess child : this.children.get(parent))
+			this.parents.put(child, null);
+		this.parents.remove(parent);
+		this.allParents.remove(parent);
+	}
+	
+	public IParentProcess newParent() throws CMException
+	{
+		IParentProcess proc = this.newParentI();
+		this.allParents.add(proc);
+		this.children.put(proc, new Vector<IChildProcess>());
+		return proc;
+	}
+	
+	protected abstract void deleteParentI(IParentProcess parent) throws CMException;
+	protected abstract IParentProcess newParentI() throws CMException;
+	protected abstract void setParentI(IChildProcess child, IParentProcess parent) throws CMException;
+
+	private Vector<IChildProcess> allChildren;
+	private Vector<IParentProcess> allParents = new Vector<IParentProcess>();
+	private HashMap<IParentProcess, Vector<IChildProcess>> children = new HashMap<IParentProcess, Vector<IChildProcess>>();
+	private HashMap<IChildProcess, IParentProcess> parents = new HashMap<IChildProcess, IParentProcess>();
+}
---
+++ complex/mixture/controllers/MHMMController.java
@@ -0,0 +1,170 @@
+package complex.mixture.controllers;
+
+import java.util.HashSet;
+import java.util.Vector;
+
+import bn.BNException;
+import bn.distributions.DiscreteCPT;
+import bn.distributions.DiscreteCPTUC;
+import bn.dynamic.IDBNNode;
+import bn.dynamic.IDynamicBayesNet;
+import bn.dynamic.IFDiscDBNNode;
+import bn.messages.FiniteDiscreteMessage;
+import complex.CMException;
+import complex.featural.IChildProcess;
+import complex.featural.IParentProcess;
+import complex.mixture.MixtureModelController;
+
+public class MHMMController extends MixtureModelController {
+	
+	public MHMMController(IDynamicBayesNet network, Vector<MHMMChild> children, MHMMParameterPrior priors, int ns)
+	{
+		super(children);
+		this.network = network;
+		this.priors = priors;
+		this.ns = ns;
+	}
+	
+	public static interface MHMMChild extends IChildProcess
+	{
+		IDBNNode hook();
+		
+		void optimize(Vector<FiniteDiscreteMessage> incPis);
+		double evaluateP();
+		void sampleInit();
+		void samplePosterior();
+	}
+	
+	private static class MHMMX implements IParentProcess
+	{
+		MHMMX(IFDiscDBNNode xnd,int ID)
+		{
+			this.xnd = xnd;
+			this.ID = ID;
+		}
+		public String getName()
+		{
+			return this.xnd.getName();
+		}
+		public FiniteDiscreteMessage marginal(int t)
+		{
+			try {
+				return this.xnd.getMarginal(t);
+			} catch(BNException e) {
+				System.err.println("Error : " + e.toString());
+				return null;
+			}
+		}
+		int ID;
+		IFDiscDBNNode xnd;
+	}
+	
+	public static interface MHMMParameterPrior
+	{
+		public double evaluate(DiscreteCPT A);
+		public double evaluate(DiscreteCPTUC pi);
+		
+		public DiscreteCPT initialSampleA();
+		public DiscreteCPTUC initialSamplePi();
+		public DiscreteCPT posteriorSampleA(DiscreteCPT A);
+		public DiscreteCPTUC posteriorSamplePi(DiscreteCPTUC pi);
+	}
+
+	@Override
+	protected void deleteParentI(IParentProcess parent) throws CMException
+	{
+		if(this.getChildren(parent).size()!=0)
+			throw new CMException("Cannot have an orphaned observation sequence in an mHMM!");
+		try {
+			this.network.removeNode(parent.getName());
+			this.killID(((MHMMX)parent).ID);
+		} catch(BNException e) {
+			throw new CMException("Failed to remove node " + parent.getName() + " : " + e.toString());
+		}
+	}
+
+	@Override
+	protected IParentProcess newParentI() throws CMException {
+		try {
+			int id = this.nextID();
+			IFDiscDBNNode newp = network.addDiscreteNode("X"+id, this.ns);
+			newp.setInitialDistribution(this.priors.initialSamplePi());
+			newp.setAdvanceDistribution(this.priors.initialSampleA());
+			this.network.addInterEdge(newp, newp);
+			MHMMX parent = new MHMMX(newp, id);
+			return parent;
+		} catch(BNException e) {
+			throw new CMException("Failed to add a new node to network!");
+		}
+	}
+
+	@Override
+	protected void setParentI(IChildProcess child, IParentProcess parent)
+			throws CMException
+	{
+		if(child instanceof MHMMChild && parent instanceof MHMMX)
+		{
+			try {
+				MHMMChild mchild = (MHMMChild)child;
+				MHMMX mp = (MHMMX)parent;
+				Vector<IDBNNode> parents = new Vector<IDBNNode>();
+				for(IDBNNode parentnd : mchild.hook().getIntraParents())
+					parents.add(parentnd);
+				if(parents.size()>1)
+					throw new BNException("Expected child " + child.getName() + " to have one and only one parent!");
+				if(parents.size()==1)
+					this.network.removeIntraEdge(parents.get(0).getName(), mchild.hook().getName());
+				this.network.addIntraEdge(mp.xnd.getName(), mchild.hook().getName());
+			} catch(BNException e) {
+				throw new CMException("Error changing parent for node " + child.getName() + " : " + e.toString());
+			}
+		}
+		else
+			throw new CMException("Attempted to change parent of non-MHMM observation process.");
+	}
+	
+	@Override
+	public void optimizeChildParameters(IChildProcess child)
+	throws CMException {
+		if(child instanceof MHMMChild)
+		{
+			try {
+				MHMMChild mchild = (MHMMChild)child;
+				MHMMX parent = (MHMMX)this.getParent(child);
+				Vector<FiniteDiscreteMessage> pis = new Vector<FiniteDiscreteMessage>();
+				for(int t = 0; t < this.network.getT(); t++)
+					pis.add(parent.xnd.getMarginal(t));
+				mchild.optimize(pis);
+			} catch(BNException e) {
+				throw new CMException("Failed to optimize child node...");
+			}
+		}
+		else throw new CMException("Attempted to optimize non-mHMM observation process.");
+	}
+	
+	private int nextID()
+	{
+		int ret = this.minimum_available_id;
+		this.usedIDs.add(ret);
+		while(true)
+		{
+			this.minimum_available_id++;
+			if(!this.usedIDs.contains(this.minimum_available_id))
+				break;
+		}
+		return ret;
+	}
+	
+	private void killID(int id)
+	{
+		this.usedIDs.remove(id);
+		if(id < this.minimum_available_id)
+			this.minimum_available_id = id;
+	}
+	
+	private int ns;
+	private HashSet<Integer> usedIDs = new HashSet<Integer>();
+	private int minimum_available_id = 0;
+	private MHMMParameterPrior priors;
+
+}

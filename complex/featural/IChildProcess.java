ac911eb3f5a0671fd39add0040d6ac65ab775f19
nsandell <nsandell@d05d5076-8e11-4e8a-86b5-5794bff6af64>
2011-03-08 22:53:30 +0000

    Children should now be able to backup their parameters for reverts

--- complex/featural/IChildProcess.java
+++ complex/featural/IChildProcess.java
@@ -1,7 +1,10 @@
 package complex.featural;
 
+import complex.CMException;
 import complex.metrics.Coherence.DisagreementMeasure;
 
 public interface IChildProcess extends DisagreementMeasure{
 	String getName();
+	void backupParameters() throws CMException;
+	void restoreParameters() throws CMException;
 }

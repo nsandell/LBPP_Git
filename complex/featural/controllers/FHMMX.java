4ccf0d776ac8aa3bb707087da4bdaefe691baa16
nsandell <nsandell@d05d5076-8e11-4e8a-86b5-5794bff6af64>
2011-03-29 14:06:17 +0000

    Now can backup parameters

--- complex/featural/controllers/FHMMX.java
+++ complex/featural/controllers/FHMMX.java
@@ -1,6 +1,7 @@
 package complex.featural.controllers;
 
 import bn.BNException;
+import bn.distributions.DiscreteDistribution.DiscreteFiniteDistribution;
 import bn.dynamic.IFDiscDBNNode;
 import bn.messages.FiniteDiscreteMessage;
 
@@ -13,10 +14,12 @@ public class FHMMX implements IParentProcess
 		this.xnd = xnd;
 		this.ID = ID;
 	}
+	
 	public String getName()
 	{
 		return this.xnd.getName();
 	}
+	
 	public FiniteDiscreteMessage marginal(int t)
 	{
 		try {
@@ -26,6 +29,30 @@ public class FHMMX implements IParentProcess
 			return null;
 		}
 	}
+	
+	@Override
+	public void backupParameters() {
+		if(!this.xnd.isLocked())
+		{
+			this.Abackup = xnd.getAdvanceDistribution();
+			this.pibackup = xnd.getInitialDistribution();
+		}
+	}
+	@Override
+	public void restoreParameters() {
+		try {
+		if(!this.xnd.isLocked() && this.Abackup != null && this.pibackup !=null)
+		{
+			this.xnd.setAdvanceDistribution(this.Abackup);
+			this.xnd.setInitialDistribution(this.pibackup);
+		}
+		} catch(BNException e) {
+			System.err.println("Failed to restore parameters for node " + this.getName());
+		}
+	}
+	
 	int ID;
 	IFDiscDBNNode xnd;
+	private DiscreteFiniteDistribution Abackup;
+	private DiscreteFiniteDistribution pibackup;
 }

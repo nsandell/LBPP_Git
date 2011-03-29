6c42eba39d8e641ac00ad4de5a23a99a2ad7d3c9
nsandell <nsandell@d05d5076-8e11-4e8a-86b5-5794bff6af64>
2011-03-29 14:05:10 +0000

    Give a node a unique parent - node chosen by how poorly it fits its parents.

---
+++ complex/featural/proposal_generators/CoherenceUniqueParenter.java
@@ -0,0 +1,53 @@
+package complex.featural.proposal_generators;
+
+import java.util.Vector;
+
+import util.MathUtil;
+
+import complex.IParentProcess;
+import complex.featural.FeaturalModelController;
+import complex.featural.IFeaturalChild;
+import complex.featural.ProposalAction;
+import complex.featural.ProposalGenerator;
+import complex.metrics.Coherence;
+
+public class CoherenceUniqueParenter<ChildProcess extends IFeaturalChild, ParentProcess extends IParentProcess> implements ProposalGenerator<ChildProcess,ParentProcess> {
+	
+	public CoherenceUniqueParenter(double pcspl, double prspl, double prmerge, double pdmerge)
+	{
+		this.pcspl = pcspl; this.prspl = prspl; this.prmerge = prmerge; this.pdmerge = pdmerge;
+	}
+	double pcspl; double prspl; double prmerge; double pdmerge;
+
+	@Override
+	public Proposal<ChildProcess,ParentProcess> generate(FeaturalModelController<ChildProcess,ParentProcess> cont) {
+		
+		Vector<ChildProcess> children = cont.getObservedNodes();
+		double[] coherence = new double[children.size()];
+		double sum = 0;
+		for(int i = 0; i < coherence.length; i++) {
+			for(int t  = 0; t < cont.getT(); t++)
+				coherence[i] = children.get(i).getDisagreement(t);
+			coherence[i] += smoothness;
+				
+			//Coherence.coherence(children,cont.getT());
+			sum += coherence[i];
+		}
+		for(int i = 0; i < coherence.length; i++)
+			coherence[i] /= sum;
+		
+		int selection = MathUtil.discreteSample(coherence);
+		System.out.println("Attempting to give " + children.get(selection).getName() + " a unique parent based on incoherence.");
+		double fp = this.pcspl*coherence[selection] + this.prspl/coherence.length;
+		double bp = this.prmerge/coherence.length/(coherence.length+1); //TODO determine if we want to exactly determine reverse move probability
+	
+		return new Proposal<ChildProcess,ParentProcess>(fp,bp,new ProposalAction.UniqueParentAddAction<ChildProcess, ParentProcess>(children.get(selection)));
+	}
+
+	@Override
+	public String name() {
+		return "Disagreement-based unique parenter";
+	}
+
+	public double smoothness = 0;
+}

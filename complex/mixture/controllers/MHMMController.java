25b74222ec6fa45a88f47e6a8d90b200727062c6
nsandell <nsandell@d05d5076-8e11-4e8a-86b5-5794bff6af64>
2011-03-29 14:00:26 +0000

    Added parameter locking option, ability to run and learn chains,

--- complex/mixture/controllers/MHMMController.java
+++ complex/mixture/controllers/MHMMController.java
@@ -23,6 +23,13 @@ public class MHMMController extends MixtureModelController<MHMMChild,MHMMX> {
 		this.priors = priors;
 		this.ns = ns;
 	}
+	
+	public MHMMController(IDynamicBayesNet network, Vector<MHMMChild> children, MHMMParameterPrior priors, int ns, boolean lockMCParams)
+	{
+		this(network,children,priors,ns);
+		this.lockMCParams = lockMCParams;
+	}
+	private boolean lockMCParams =  false;
 
 	public static interface MHMMParameterPrior
 	{
@@ -31,7 +38,7 @@ public class MHMMController extends MixtureModelController<MHMMChild,MHMMX> {
 
 		public DiscreteCPT initialSampleA();
 		public DiscreteCPTUC initialSamplePi();
-		public DiscreteCPT posteriorSampleA(DiscreteCPT A);
+		public DiscreteCPT posteriorSampleA(DiscreteCPT A, int T);
 		public DiscreteCPTUC posteriorSamplePi(DiscreteCPTUC pi);
 	}
 
@@ -42,6 +49,7 @@ public class MHMMController extends MixtureModelController<MHMMChild,MHMMX> {
 			throw new CMException("Cannot have an orphaned observation sequence in an mHMM!");
 		try {
 			this.network.removeNode(parent.getName());
+			this.parentNames.remove(parent.getName());
 			this.killID(((MHMMX)parent).ID);
 		} catch(BNException e) {
 			throw new CMException("Failed to remove node " + parent.getName() + " : " + e.toString());
@@ -58,6 +66,8 @@ public class MHMMController extends MixtureModelController<MHMMChild,MHMMX> {
 			this.network.addInterEdge(newp, newp);
 			MHMMX parent = new MHMMX(newp, id);
 			this.parentNames.add("X"+id);
+			if(this.lockMCParams)
+				parent.xnd.lockParameters();
 			return parent;
 		} catch(BNException e) {
 			throw new CMException("Failed to add a new node to network!");
@@ -135,13 +145,44 @@ public class MHMMController extends MixtureModelController<MHMMChild,MHMMX> {
 			nodes.addAll(child.constituentNodeNames());
 
 		try {
-			this.network.run_parallel_block(nodes,maxit,conv);
+			//this.network.run_parallel_block(nodes,maxit,conv);
+			this.network.run_parallel_queue(maxit,conv,nodes);
 			double ll = this.network.getLogLikelihood();
 
 			if(Double.isNaN(ll) || ll > 0)
 			{
 				this.network.resetMessages();
 				this.network.run_parallel_block(maxit,conv);
+				ll = this.network.getLogLikelihood();
+				if(Double.isNaN(ll) || ll > 0)
+				{
+					this.log("NAN LIKELIHOOD");
+					this.log(this.network.toString());
+					throw new CMException("Model returns NaN/Greater than 0 log likelihood!");
+				}
+			}
+			return ll;
+		} catch(BNException e) {
+			throw new CMException(e.toString());
+		}
+	}
+	
+	public double learnChain(MHMMX proc, int maxrun, double runconv, int maxlearn, double learnconv) throws CMException
+	{
+		Vector<String> nodes = new Vector<String>();
+		nodes.add(proc.xnd.getName());
+		for(MHMMChild child : this.getChildren(proc))
+			nodes.addAll(child.constituentNodeNames());
+		
+		try {
+			this.network.optimize_parallel(maxlearn, learnconv, maxrun, runconv,nodes);
+			double ll = this.network.getLogLikelihood();
+			
+			if(Double.isNaN(ll) || ll > 0)
+			{
+				System.err.println("Encountered NaN/Greater than 0 ll, attempting to recover.");
+				this.network.resetMessages();
+				this.network.run_parallel_block(maxrun,runconv);
 				ll = this.network.getLogLikelihood();
 				if(Double.isNaN(ll) || ll > 0)
 				{

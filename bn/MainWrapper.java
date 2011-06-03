57f799c330abacdd00a9a1d9ecde5f50c4d4b391
nsandell <nsandell@d05d5076-8e11-4e8a-86b5-5794bff6af64>
2011-06-03 20:55:31 +0000

    Decided to add back mainwrapper class for backwards compatibility

---
+++ bn/MainWrapper.java
@@ -0,0 +1,14 @@
+package bn;
+
+/**
+ * This class simply starts up the command line interfaces, either static or dynamic depending on
+ * the arguments.
+ * @author Nils F. Sandell
+ */
+public class MainWrapper {
+	
+	public static void main(String[] args)
+	{
+		CommandLine.main(args);
+	}
+}
--- tests/MHMMTestLearn.java
+++ tests/MHMMTestLearn.java
@@ -1,187 +1,31 @@
 package tests;
 
-import java.util.Collection;
-
-import java.util.Iterator;
-
 import java.util.Vector;
 
 import bn.BNException;
 import bn.distributions.DiscreteCPT;
 import bn.distributions.DiscreteCPTUC;
-import bn.dynamic.IDBNNode;
 import bn.dynamic.IDynamicBayesNet;
 import bn.dynamic.IFDiscDBNNode;
 import bn.impl.dynbn.DynamicNetworkFactory;
-import bn.messages.FiniteDiscreteMessage;
-import bn.messages.MessageSet;
 import complex.CMException;
-import complex.IParentProcess;
-import complex.mixture.FixedMixture;
-import complex.mixture.FixedMixture.FMModelOptions;
+import complex.latents.FiniteMarkovChain;
+import complex.mixture.DirichletMixture;
+import complex.mixture.IMixtureChild;
+import complex.mixture.DirichletMixture.DMModelOptions;
 import complex.mixture.controllers.MHMMController;
-import complex.mixture.controllers.MHMMChild;
-import complex.mixture.controllers.MHMMX;
-import complex.mixture.controllers.MHMMController.MHMMParameterPrior;
 
 public class MHMMTestLearn {
 	
-	private static class Priors implements MHMMParameterPrior
+	public static class BasicDiscreteChild extends IMixtureChild.MixtureSingleNodeChild
 	{
-
-		@Override
-		public double evaluate(DiscreteCPT A) {
-			return 1;
-		}
-
-		@Override
-		public double evaluate(DiscreteCPTUC pi) {
-			// TODO Auto-generated method stub
-			return 1;
-		}
-
-		@Override
-		public DiscreteCPT initialSampleA() {
-			try {
-				return new DiscreteCPT(new double[][]{{.9,.1},{.1,.9}}, 2);
-			} catch(BNException e) {
-				System.err.println("Error sampling A matrix!");
-				return null;
-			}
-		}
-
-		@Override
-		public DiscreteCPTUC initialSamplePi() {
-			try {
-				return new DiscreteCPTUC(new double[]{.99,.01});
-			} catch(BNException e) {
-				System.err.println("Error sampling pi!");
-				return null;
-			}
-		}
-
-		@Override
-		public DiscreteCPT posteriorSampleA(DiscreteCPT A, int T) {
-			return A;
-		}
-
-		@Override
-		public DiscreteCPTUC posteriorSamplePi(DiscreteCPTUC pi) {
-			return pi;
-		}
-		
-	}
-	
-	private static class SingularFDMS implements MessageSet<FiniteDiscreteMessage>
-	{
-		public SingularFDMS(FiniteDiscreteMessage sing) {
-			this.msg = sing;
-		}
-		
-		private static class SingIt implements Iterator<FiniteDiscreteMessage>
+		public BasicDiscreteChild(IFDiscDBNNode node)
 		{
-			public SingIt(FiniteDiscreteMessage msg)
-			{
-				this.msg = msg;
-			}
-			FiniteDiscreteMessage msg;
-
-			@Override
-			public boolean hasNext() {
-				return msg!=null;
-			}
-
-			@Override
-			public FiniteDiscreteMessage next() {
-				FiniteDiscreteMessage tmp = msg;
-				msg = null;
-				return tmp;
-			}
-
-			@Override
-			public void remove() throws UnsupportedOperationException {throw new UnsupportedOperationException();}
+			super(node);
 		}
-		
-		private FiniteDiscreteMessage msg;
-
-		@Override
-		public Iterator<FiniteDiscreteMessage> iterator() {
-			return new SingIt(msg);
-		}
-
-		@Override
-		public int size() {
-			return this.msg==null ? 0 : 1;
-		}
-
-		@Override
-		public FiniteDiscreteMessage get(int index) {
-			return index==0 ? msg : null; 
-		}
-
-		@Override
-		public void remove(int index) {
-			this.msg = index==0 ? null : this.msg;
-		}
-
-		@Override
-		public void removeAll() {
-			this.msg = null;
-		}
-	}
-	
-	private static class MHMMCTest implements MHMMChild
-	{
-		public MHMMCTest(IFDiscDBNNode node)
-		{
-			this.node = node;
-		}
-		IFDiscDBNNode node;
-		
-		public Collection<String> constituentNodeNames()
-		{
-			Vector<String> names = new Vector<String>();
-			names.add(node.getName());
-			return names;
-		}
-		
-		@Override
-		public String getName() {
-			return node.getName();
-		}
-
-		@Override
-		public double getDisagreement(int t) {
-			return node.conditionalLL(t);
-		}
-
-		@Override
-		public IDBNNode hook() {
-			return node;
-		}
-
-		@Override
-		public void optimize()
-		{
-			try {
-				this.node.optimizeParameters();
-			} catch(BNException e) {
-				System.err.println("Failed to optimized node " + this.getName() + ": " + e.toString());
-			}
-		}
-
-		@Override
-		public void backupParameters() {}
-
-		@Override
-		public void restoreParameters() {}
-
-		@Override
-		public void setParent(IParentProcess rent) {}
 
 		@Override
 		public double parameterLL() {
-			// TODO Auto-generated method stub
 			return 0;
 		}
 	}
@@ -190,7 +34,7 @@ public class MHMMTestLearn {
 	 * @param args
 	 */
 	public static void main(String[] args) throws BNException, CMException {
-		IDynamicBayesNet network = DynamicNetworkFactory.newDynamicBayesNet(600);
+		IDynamicBayesNet network = DynamicNetworkFactory.newDynamicBayesNet(36);
 		
 		int[][] o = new int[][]{
 				{0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -202,21 +46,28 @@ public class MHMMTestLearn {
 				{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
 				{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
 				{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0}
+				{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0}
 		};
 		
-		Vector<MHMMChild> children = new Vector<MHMMChild>();
+		Vector<BasicDiscreteChild> children = new Vector<BasicDiscreteChild>();
 		for(int i = 0; i < o.length; i++)
 		{
 			IFDiscDBNNode nd = network.addDiscreteNode("Y"+i, 2);
 			nd.setAdvanceDistribution(new DiscreteCPT(new double[][]{{.9,.1},{.1,.9}},2));
 			nd.setValue(o[i], 0);
-			children.add(new MHMMCTest(nd));
+			children.add(new BasicDiscreteChild(nd));
 		}
-		MHMMController controller = new MHMMController(network, children, new Priors(), 2);
-		FMModelOptions<MHMMChild,MHMMX> opts = new FMModelOptions<MHMMChild,MHMMX>(controller,6);
+		MHMMController controller = new MHMMController(network, children, 
+				new FiniteMarkovChain.FiniteMarkovChainFactory(
+						new FiniteMarkovChain.DiracFiniteMCPrior(	new DiscreteCPT(new double[][]{{.9,.1},{.1,.9}}, 2),
+																	new DiscreteCPTUC(new double[]{.9,.1}),2)));
+		DMModelOptions opts = new DMModelOptions(controller, 1);
+		opts.controller.setTrace(null);
 		opts.controller.setLogger(System.out);
-		FixedMixture.learnFixedMixture(opts);
+		opts.maxIterations = 50;
+		DirichletMixture.learnDirichletMixture(opts);
 	}
 
 }
